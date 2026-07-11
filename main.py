0dbb84b1b3b93cdfc64c9fbc65871f629ac03026
Jason <hnhjknmn@gmail.com>
2026-07-11 13:30:50 -0400

    random ai, copied the minimax from my previous tic tac toe program

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from BoardFile import *
+import random
+
 running = True
 player = -1
 # 1 is ai -1 is human
@@ -12,6 +14,47 @@ board = [
     [".", ".", ".", ".", ".", ".", "."],
     ["X", ".", ".", ".", ".", ".", "."],
 ]
+
+def random_ai(board):
+    valid_cols = []
+
+    for i in range(7):
+        if board[0][i] == ".":
+            valid_cols.append(i)
+
+    col = random.choice(valid_cols)
+
+    for i in range(5, -1, -1):
+        if board[i][col] == ".":
+            board[i][col] = "X" 
+            return
+
+def ai_turn(board):
+    best_score = -999
+    best_move = None
+
+    for pos in range(len(board)):
+        if board[pos] == "?":
+            board[pos] = "X"
+            score = minimax(board, -1)
+            board[pos] = "?"
+            
+            if score > best_score:
+                best_score = score
+                best_move = pos
+
+    if best_move is not None:
+        board[best_move] = "X"
+
+    for i in range(5,-1,-1):
+        for i in range(7)
+            if board[i][col] == ".":
+                if player == 1:
+                    board[i][col] = "X"
+                    break
+                else:
+                    board[i][col] = "O"
+                    break
 
 def pvp_turn(board):
     global player
@@ -45,7 +88,8 @@ def choose_mode():
     while True:
         try:
             print("Choose a mode")
-            print("1/vs Human, 2/vs AI")
+            print("1. vs Human")
+            print("2. vs random AI")
             mode1 = int(input())
 
             if mode1 not in valid:
